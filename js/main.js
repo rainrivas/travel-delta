17ae4e3a6b96d40f0df6fd31c8c852165858da78
jwu910 <jwu910@yahoo.com>
2017-05-29 18:26:34 -0700

    adjusted spacing for readability.

--- js/main.js
+++ js/main.js
@@ -1,37 +1,47 @@
 YUI().use(
 	'aui-form-validator',
 	function(Y) {
-		var rules = {
-			locationName: {
-				required: true
-			},
-			locationAddress: {
-				required: true
-			},
-			locationCity: {
-				required: true
-			},
-			locationZip: {
-				minLength: 5,
-				maxLength: 5,
-				required: true
-			}
+		var rules = 
+			{
+				locationName: 
+					{
+						required: true
+					},
+				locationAddress: 
+					{
+						required: true
+					},
+				locationCity: 
+					{
+						required: true
+					},
+				locationZip: 
+					{
+						minLength: 5,
+						maxLength: 5,
+						required: true
+					}
 		};
 
-		var fieldStrings = {
-			locationName: {
-				required: 'Nickname for this location.'
-			},
-			locationAddress: {
-				required: 'Address of this location.'
-			},
-			locationCity: {
-				required: 'City for this location.'
-			},
-			locationZip: {
-				required: 'Must be a valid zip code.'
-			}
-		};
+		var fieldStrings = 
+			{
+				locationName: 
+					{
+						required: 'Nickname for this location.'
+					},
+				locationAddress: 
+					{
+						required: 'Address of this location.'
+					},
+				locationCity: 
+					{
+						required: 'City for this location.'
+					},
+				locationZip: 
+					{
+						required: 'Must be a valid zip code.'
+					}
+			};
 
 		new Y.FormValidator({
 			boundingBox: '#locationGroups',
